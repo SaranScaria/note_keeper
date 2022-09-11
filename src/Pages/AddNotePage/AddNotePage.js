5ba3dafaa74c788be75c80dd8c8b4f87611494b5
SaranScaria <111234633+SaranScaria@users.noreply.github.com>
2022-09-11 22:58:39 +0530

    Update AddNotePage.js

--- src/Pages/AddNotePage/AddNotePage.js
+++ src/Pages/AddNotePage/AddNotePage.js
@@ -7,7 +7,7 @@ function AddNotePage() {
   return (
     <div>
       <div className='box'>
-        <button className='close_button' onClick={()=>navigate('/')}>X</button>
+        <button className='close_button' onClick={()=>navigate('/note_keeper')}>X</button>
         <img className='notepad' src="https://www.papertraildesign.com/wp-content/uploads/2021/01/Notes.jpg" alt="" />
         <button className='save'>SAVE</button>
         <button className='delete'>DELETE</button>
